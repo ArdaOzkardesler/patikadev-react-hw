9f445849b5464b8be33e4fad33442c83eefc1d25
Arda Ozkardesler <aozkardesler@gmail.com>
2021-10-26 23:08:14 +0300

    hw1 done

--- hw1/app.js
+++ hw1/app.js
@@ -1,1 +1,16 @@
+import axios from "axios";
 
+async function getData(userID) {
+  const { data: user } = await axios(
+    `https://jsonplaceholder.typicode.com/users/${userID}`
+  );
+
+  const { data: post } = await axios(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userID}`
+  );
+
+  const merged = { ...user, ...post };
+  return merged;
+}
+
+export default getData;
