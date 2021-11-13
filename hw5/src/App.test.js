f1eaff3a982971b3acad239aad844d80a4bd04d0
Arda Ozkardesler <aozkardesler@gmail.com>
2021-11-13 16:08:38 +0300

    hw5 done

--- hw5/src/App.test.js
+++ hw5/src/App.test.js
@@ -1,8 +1,39 @@
 import React from "react";
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ReactDOM from "react-dom";
+import '@testing-library/jest-dom';
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+import SearchInput from './SearchInput';
+import Header from './Header';
+import EmojiResultsRow from './EmojiResultsRow';
+import EmojiResultRow from './EmojiResultRow';
+
+describe('Testing emoji-app', () => {
+  test('header should be in document', () => {
+    render(<Header />);
+    expect(screen.getByText(/Emoji Search/i)).toBeInTheDocument();
+  });
+
+  test('emoji list should be in document', () => {
+    const { queryByTestId } = render(<EmojiResultsRow />);
+    expect(queryByTestId('component-emoji-result-row'));
+  });
+
+  test('check filtered emojis', () => {
+    render(<SearchInput />);
+    const input = screen.getByLabelText('Search');
+    const emojiFilt = 'Grin';
+    userEvent.type(input, emojiFilt);
+    expect(screen.getByText(emojiFilt)).toBeInTheDocument();
+  });
+
+  test('check copied emoji', () => {
+    render(<EmojiResultRow />);
+    const emoji = screen.getByText(/100/i);
+    userEvent.click(emoji);
+    const copyEmoji = window.ClipboardData.getData();
+    expect(copyEmoji).toEqual(emoji);
+  });
 });
