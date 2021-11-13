a180090f5379cffeeb3090e7cc4c337de2215d21
Arda Ozkardesler <aozkardesler@gmail.com>
2021-11-13 15:17:30 +0300

    hw4 done

--- hw4/kodluyoruzui/example/src/App.js
+++ hw4/kodluyoruzui/example/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
-import { ExampleComponent } from 'kodluyoruzui'
+import { Button } from 'kodluyoruzui'
 import 'kodluyoruzui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <Button text='Default' onClick={() => alert('Default')} />
+      <br />
+      <br />
+      <Button text='Primary' type='primary' onClick={() => alert('Primary')} />
+      <br />
+      <br />
+      <Button type='dashed' text='Dashed' onClick={() => alert('Dashed')} />
+      <br />
+      <br />
+      <Button text='Text' type='text' onClick={() => alert('Text')} />
+      <br />
+      <br />
+      <Button type='link' text='Link' onClick={() => alert('Link')} />
+    </>
+  )
 }
 
 export default App
